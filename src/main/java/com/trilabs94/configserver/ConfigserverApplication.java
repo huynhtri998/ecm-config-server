62c127d82fd5aa76e483dab8af0dde9eff186eea
huynhtri998 <huynhtri998@gmail.com>
2025-11-18 15:50:38 +0700

    initialize config server

---
+++ src/main/java/com/trilabs94/configserver/ConfigserverApplication.java
@@ -0,0 +1,15 @@
+package com.trilabs94.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
